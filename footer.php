8f518b6d2af5d444652a9141d723b5c5c10f5ac2
kathoreakshata <digital.cmq@gmail.com>
2023-03-11 16:56:26 +0530

    footers structure created

--- footer.php
+++ footer.php
@@ -1,4 +1,63 @@
-
+<section id="footer">
+  <div class="container-fluid">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="row">
+            <div class="col">
+              <img src="https://logodix.com/logo/413743.png" alt="mygenic logo" height="100px">
+            </div>
+            <div class="col">
+              <h5>Our Company</h5>
+              <p>About Us</p>
+              <p>Our Products</p>
+              <p>Contact Us</p>
+              <p>Download Broucher</p>
+            </div>
+          </div>
+          <div class="row">
+            <p>We Accept Online Payments</p>
+              <i class="bi bi-app"></i>
+              <i class="bi bi-app"></i>
+              <i class="bi bi-app"></i>
+              <i class="bi bi-app"></i>
+          </div>
+        </div>
+        <div class="col">
+          <h5>Reach Us</h5>
+          <p>Mygenic Healthcare Pvt. Ltd.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, quas.</p>
+          <p>Lorem, ipsum.</p>
+          <p>Lorem, ipsum dolor.</p>
+          <p>Lorem, ipsum dolor.</p>
+          <button>Lorem.</button>
+          <button>Lorem.</button>
+          <button>Lorem.</button>
+          <p>Share Us Via</p>
+          <i class="bi bi-app"></i>
+          <i class="bi bi-app"></i>
+          <i class="bi bi-app"></i>
+        </div>
+        <div class="col">
+          <h5>Leave a message we will call you back</h5>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Contact Number</label>
+            <input type="phone" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="+91 ">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Message</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Message goes here...">
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <hr>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+      </div>
+    </div>
+  </div>
+</section>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
