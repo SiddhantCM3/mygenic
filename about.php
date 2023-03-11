5ffa4ba70cb2214d0f6ff8a8d739c795350301bf
DeveloperSnehal <developer.creativemarque@gmail.com>
2023-03-11 16:14:35 +0530

    Create about-us page and contact page

---
+++ about.php
@@ -0,0 +1,187 @@
+<?php include("header.php"); ?>
+<!-----------------------about-us----------------------------------->
+    <section id="about-us">
+        <div class="container">
+             <h1 class="text-center">MYGENIC HELTHCARE PRIVATE LIMITED</h1>
+             <p></p>
+        </div>
+    </section>
+
+<!-------------------------company-album---------------------------------------->
+    <section id="about-company">
+         <div class="container">
+              <h2 class="text-center">COMPANY ALBUM</h2>
+         </div>
+    </section>
+
+<!------------------------why-us----------------------------------------------->
+     <section id="whyus">
+         <div class="container">
+             <h2 class="text-center">WHY US</h2>
+             <p class="text-center"></p>
+                 <div class="text-center">
+                     <ul class="list-group">
+                         <li class="list-group-item">Cras justo odio</li>
+                         <li class="list-group-item">Dapibus ac facilisis in</li>
+                         <li class="list-group-item">Morbi leo risus</li>
+                         <li class="list-group-item">Porta ac consectetur ac</li>
+                          <li class="list-group-item">Vestibulum at eros</li>
+                     </ul>
+                 </div>
+         </div>
+     </section>
+<!--------------------------------our products---------------------------------------------->
+     <section id="ourproducts">
+         <div class="container">
+             <div class="row">
+                 <div class="col-lg-4">
+                     <ul class="list-group">
+                         <li class="list-group-item active"><h4 class="text-center">OUR PRODUCTS</h4></li>
+                         <li class="list-group-item">
+                             <div class="dropdown">
+                                 <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                   Pharmaceutical Tablets
+                                 </button>
+                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                         <a class="dropdown-item" href="#">Action</a>
+                                         <a class="dropdown-item" href="#">Another action</a>
+                                         <a class="dropdown-item" href="#">Something else here</a>
+                                     </div>
+                             </div>
+                         </li>
+                         <li class="list-group-item">
+                         <div class="dropdown">
+                                 <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                   Pharmaceutical Tablets
+                                 </button>
+                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                         <a class="dropdown-item" href="#">Action</a>
+                                         <a class="dropdown-item" href="#">Another action</a>
+                                         <a class="dropdown-item" href="#">Something else here</a>
+                                     </div>
+                             </div>
+                         </li>
+                          <li class="list-group-item">
+                          <div class="dropdown">
+                                 <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                   Pharmaceutical Tablets
+                                 </button>
+                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                         <a class="dropdown-item" href="#">Action</a>
+                                         <a class="dropdown-item" href="#">Another action</a>
+                                         <a class="dropdown-item" href="#">Something else here</a>
+                                     </div>
+                             </div>
+                          </li>
+                          <li class="list-group-item">
+                          <div class="dropdown">
+                                 <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                   Pharmaceutical Tablets
+                                 </button>
+                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                         <a class="dropdown-item" href="#">Action</a>
+                                         <a class="dropdown-item" href="#">Another action</a>
+                                         <a class="dropdown-item" href="#">Something else here</a>
+                                     </div>
+                             </div>
+                          </li>
+                          <li class="list-group-item">
+                          <div class="dropdown">
+                                 <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                   Pharmaceutical Tablets
+                                 </button>
+                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                         <a class="dropdown-item" href="#">Action</a>
+                                         <a class="dropdown-item" href="#">Another action</a>
+                                         <a class="dropdown-item" href="#">Something else here</a>
+                                     </div>
+                             </div>
+                          </li>
+                          <li class="list-group-item">
+                          <div class="dropdown">
+                                 <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                   Pharmaceutical Tablets
+                                 </button>
+                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                         <a class="dropdown-item" href="#">Action</a>
+                                         <a class="dropdown-item" href="#">Another action</a>
+                                         <a class="dropdown-item" href="#">Something else here</a>
+                                     </div>
+                             </div>
+                          </li>
+                      </ul>
+                  </div>
+                 <!-------------second-colum----------------->
+                  <div class="col-lg-8">
+                     <h3 class="text-center">FACTSHEET</h3>
+                     <div class="container">
+                         <div class="row">
+                              <div class="col-sm">
+                                 One of three columns
+                              </div>
+                                  <div class="row">
+                                     <div class="col-lg-6">
+                                         1
+                                      </div>
+                                      <div class="col-lg-6">
+                                          1
+                                      </div>
+                                      <div class="col-lg-6">
+                                         2
+                                      </div>
+                                      <div class="col-lg-6">
+                                         2
+                                      </div>
+                                      <div class="col-lg-6">
+                                         3
+                                      </div>
+                                      <div class="col-lg-6">
+                                         3
+                                      </div>
+                                      <div class="col-lg-6">
+                                         4
+                                      </div>
+                                      <div class="col-lg-6">
+                                          4
+                                      </div>
+                                      <div class="col-lg-6">
+                                         5
+                                      </div>
+                                      <div class="col-lg-6">
+                                         5
+                                      </div>
+                                      <div class="col-lg-6">
+                                         6
+                                      </div>
+                                      <div class="col-lg-6">
+                                          6
+                                      </div>
+                                      <div class="col-lg-6">
+                                          7
+                                      </div>
+                                      <div class="col-lg-6">
+                                         7
+                                      </div>
+                                      <div class="col-lg-6">
+                                          8
+                                      </div>
+                                      <div class="col-lg-6">
+                                          8
+                                      </div>
+                                      <div class="col-lg-6">
+                                          9
+                                      </div>
+                                      <div class="col-lg-6">
+                                          9
+                                      </div>
+                                 </div>
+                         </div>
+                     </div>
+                  </div>
+             </div>
+          </div>
+     </section>
+
+
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,56 @@
+<?php include("header.php"); ?>
+<body>
+<!-------------------contact-img---------------------------->
+    <section id="contact-img">
+         <div class="container-fluid">
+              <img src="...." alt="...">
+          </div>
+    </section>
+
+<!--------------------contact-Form------------------------------------->
+    <section id="contact-img">
+          <div class="container">
+               <h1 class="text-center">CONTACT US</h1>
+              <div class="row">
+                  <div class="col-lg-6">
+                     <h3>REACH US</h3>
+                     <P>My Genic healthcare private limited</P>
+                     <P>Office:- </P>
+                     <P>Namarata Patil (Manager)</P>
+                     <P>Call- 08043851554 <br><span>We Accept Online Payments</span></P>
+                     <p>Share us Via</p>
+                     <p><i class="bi bi-credit-card-2-front-fill"></i></p>
+                  </div>
+                  <div class="col-lg-6">
+                     <h3> GET IN TOUCH WITH US</h3>
+                     <div class="container">
+                          <div class="row">
+                             <div class="col-lg-12">
+                                 <input type="text" name="Full Name" placeholder="FULL NAME">
+                             </div>
+                              <div class="col-lg-12">
+                                  <input type="email" name="Email" placeholder="EMAIL">
+                             </div>
+                             <div class="col-lg-12">
+                                  <input type="phone" name="Phone" placeholder="PHONE NO.">
+                              </div>
+                              <div class="col-lg-12">
+                                  <input type="text" name="subject" placeholder="SUBJECT">
+                              </div>
+                              <div class="col-lg-12">
+                                  <input type="text" name="MASSAGE" placeholder="MASSAGE">
+                              </div>
+                           </div>
+                      </div>
+                  </div>
+             </div>
+          </div>
+     </section>
+
+<!-------------------------------Map------------------------------------------>
+<section>
+<div class="contact-map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.0934043340653!2d72.89505411382925!3d19.10355788707209!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c8760d6311c9%3A0x8bb9589f83ffcc35!2sKothari%20Compound%2C%20Jangaleshwar%20Mahadev%20Mandir%20Rd%2C%20Subhash%20Nagar%2C%20Mohili%2C%20Asalpha%2C%20Mumbai%2C%20Maharashtra%20400072!5e0!3m2!1sen!2sin!4v1678098653196!5m2!1sen!2sin" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+</section>
+</body>
