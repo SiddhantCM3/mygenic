e516eda9105c3c2c7e71565831b052f04165f6dc
DeveloperSnehal <developer.creativemarque@gmail.com>
2023-03-13 17:50:18 +0530

    content added in about and contact page

--- about.php
+++ about.php
@@ -1,9 +1,10 @@
 <?php include("header.php"); ?>
+<?php include("carousel.php"); ?>
 <!-----------------------about-us----------------------------------->
     <section id="about-us">
         <div class="container">
              <h1 class="text-center">MYGENIC HELTHCARE PRIVATE LIMITED</h1>
-             <p></p>
+             <p class="text-center">Established at Navi Mumbai (Maharashtra), We,“Mygenic Healthcare Private Limited” are one of the leading Exporter of a premium range of Pharmaceutical Tablets, Pharmaceutical Injection & Anti Cancer Capsules and many more</p>
         </div>
     </section>
 
@@ -17,171 +18,159 @@
 <!------------------------why-us----------------------------------------------->
      <section id="whyus">
          <div class="container">
-             <h2 class="text-center">WHY US</h2>
-             <p class="text-center"></p>
-                 <div class="text-center">
-                     <ul class="list-group">
-                         <li class="list-group-item">Cras justo odio</li>
-                         <li class="list-group-item">Dapibus ac facilisis in</li>
-                         <li class="list-group-item">Morbi leo risus</li>
-                         <li class="list-group-item">Porta ac consectetur ac</li>
-                          <li class="list-group-item">Vestibulum at eros</li>
+             <h2 class="text-center">WHY US ?</h2>
+             <p class="text-center">The key points for which we have won notice from our buyers include:</p>
+                 <div>
+                     <ul>
+                         <li>Experience & core knowledge</li>
+                          <li>Client-centric approach</li>
+                         <li>Fair dealings</li>
+                         <li>Ethical business dealings</li>
+                         <li>Competitive pricing</li>
                      </ul>
-                 </div>
+                  </div>
          </div>
      </section>
 <!--------------------------------our products---------------------------------------------->
-     <section id="ourproducts">
-         <div class="container">
+<section id="ourproducts">
+     <div class="container">
              <div class="row">
-                 <div class="col-lg-4">
-                     <ul class="list-group">
-                         <li class="list-group-item active"><h4 class="text-center">OUR PRODUCTS</h4></li>
-                         <li class="list-group-item">
-                             <div class="dropdown">
-                                 <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                   Pharmaceutical Tablets
-                                 </button>
-                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                         <a class="dropdown-item" href="#">Action</a>
-                                         <a class="dropdown-item" href="#">Another action</a>
-                                         <a class="dropdown-item" href="#">Something else here</a>
-                                     </div>
-                             </div>
-                         </li>
-                         <li class="list-group-item">
-                         <div class="dropdown">
-                                 <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                   Pharmaceutical Tablets
-                                 </button>
-                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                         <a class="dropdown-item" href="#">Action</a>
-                                         <a class="dropdown-item" href="#">Another action</a>
-                                         <a class="dropdown-item" href="#">Something else here</a>
-                                     </div>
-                             </div>
-                         </li>
-                          <li class="list-group-item">
-                          <div class="dropdown">
-                                 <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                   Pharmaceutical Tablets
-                                 </button>
-                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                         <a class="dropdown-item" href="#">Action</a>
-                                         <a class="dropdown-item" href="#">Another action</a>
-                                         <a class="dropdown-item" href="#">Something else here</a>
-                                     </div>
-                             </div>
-                          </li>
-                          <li class="list-group-item">
-                          <div class="dropdown">
-                                 <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                   Pharmaceutical Tablets
-                                 </button>
-                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                         <a class="dropdown-item" href="#">Action</a>
-                                         <a class="dropdown-item" href="#">Another action</a>
-                                         <a class="dropdown-item" href="#">Something else here</a>
-                                     </div>
-                             </div>
-                          </li>
-                          <li class="list-group-item">
-                          <div class="dropdown">
-                                 <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                   Pharmaceutical Tablets
-                                 </button>
-                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                         <a class="dropdown-item" href="#">Action</a>
-                                         <a class="dropdown-item" href="#">Another action</a>
-                                         <a class="dropdown-item" href="#">Something else here</a>
-                                     </div>
-                             </div>
-                          </li>
-                          <li class="list-group-item">
-                          <div class="dropdown">
-                                 <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                   Pharmaceutical Tablets
-                                 </button>
-                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                         <a class="dropdown-item" href="#">Action</a>
-                                         <a class="dropdown-item" href="#">Another action</a>
-                                         <a class="dropdown-item" href="#">Something else here</a>
-                                     </div>
-                             </div>
-                          </li>
-                      </ul>
-                  </div>
+                     <div class="col-lg-4">
+                     <h3 class="text-center">OUR PRODUCTS</h3>
+                     </div>
+
                  <!-------------second-colum----------------->
                   <div class="col-lg-8">
                      <h3 class="text-center">FACTSHEET</h3>
                      <div class="container">
                          <div class="row">
                               <div class="col-sm">
-                                 One of three columns
-                              </div>
+                                     Basic Information 
+                              </dCiv>
                                   <div class="row">
-                                     <div class="col-lg-6">
-                                         1
+                                     <div class="col-lg-6 col-sm-6">
+                                          Nature of Business
                                       </div>
-                                      <div class="col-lg-6">
-                                          1
+                                      <div class="col-lg-6 col-sm-6">
+                                           Exporter
                                       </div>
-                                      <div class="col-lg-6">
-                                         2
+                                      <div class="col-lg-6 col-sm-6">
+                                          Additional Business
                                       </div>
-                                      <div class="col-lg-6">
-                                         2
+                                      <div class="col-lg-6 col-sm-6">
+                                          <P>Wholesaler<br>
+                                           <span>Distributor</span><br>
+                                           <span>Service Provider</span><br>
+                                           <span>Manufacturer</P>
                                       </div>
-                                      <div class="col-lg-6">
-                                         3
+                                      <div class="col-lg-6 col-sm-6">
+                                         <p>Company CEO</p>
                                       </div>
-                                      <div class="col-lg-6">
-                                         3
+                                      <div class="col-lg-6 col-sm-6">
+                                         <p>Namrata Patil</p>
                                       </div>
-                                      <div class="col-lg-6">
-                                         4
+                                      <div class="col-lg-6 col-sm-6">
+                                         <p>Registered Address</p>
                                       </div>
-                                      <div class="col-lg-6">
-                                          4
+                                      <div class="col-lg-6 col-sm-6">
+                                          <p>Office No- 2 Plot No- 98, Sector 35 Dolphin Platinum Kamothe, Navi Mumbai, Raigarh, Navi Mumbai - 410209, Maharashtra, India</p>
                                       </div>
-                                      <div class="col-lg-6">
-                                         5
+                                      <div class="col-lg-6 col-sm-6">
+                                         <p>Total Number of Employees</p>
                                       </div>
-                                      <div class="col-lg-6">
-                                         5
+                                      <div class="col-lg-6 col-sm-6">
+                                         <p>Upto 10 People</p>
                                       </div>
-                                      <div class="col-lg-6">
-                                         6
+                                      <div class="col-lg-6 col-sm-6">
+                                         <p>Year of Establishment</p>
                                       </div>
-                                      <div class="col-lg-6">
-                                          6
+                                      <div class="col-lg-6 col-sm-6">
+                                          <p>2022</p>
                                       </div>
-                                      <div class="col-lg-6">
-                                          7
+                                      <div class="col-lg-6 col-sm-6">
+                                          <p>Legal Status of Firm</p>
                                       </div>
-                                      <div class="col-lg-6">
-                                         7
+                                      <div class="col-lg-6 col-sm-6">
+                                         <p>Limited Company (Ltd. / Pvt. Ltd.)</p>
                                       </div>
-                                      <div class="col-lg-6">
-                                          8
+                                      <div class="col-lg-6 col-sm-6">
+                                          <p>Annual Turnover</p>
                                       </div>
-                                      <div class="col-lg-6">
-                                          8
-                                      </div>
-                                      <div class="col-lg-6">
-                                          9
-                                      </div>
-                                      <div class="col-lg-6">
-                                          9
+                                      <div class="col-lg-6 col-sm-6">
+                                          <p>Rs. 10 - 25 Crore</p>
                                       </div>
                                  </div>
+                                         <div class="row">
+                                             <div class="col-sm">
+                                                  Trade & Market 
+                                             </div>
+                                              <div class="row">
+                                                   <div class="col-lg-6 col-sm-6">
+                                                         Export Percentage
+                                                   </div>
+                                                   <div class="col-lg-6 col-sm-6">
+                                                       upto 20%
+                                                   </div>
+                                              </div>
+                                         </div>
+                                         <div class="row">
+                                             <div class="col-sm">
+                                                    Statutory Profile 
+                                             </div>
+                                              <div class="row">
+                                                   <div class="col-lg-6 col-sm-6">
+                                                         Import Export Code (IEC)
+                                                   </div>
+                                                   <div class="col-lg-6 col-sm-6">
+                                                         AATCA*****
+                                                   </div>
+                                                   <div class="col-lg-6 col-sm-6">
+                                                           Tan No.
+                                                   </div>
+                                                   <div class="col-lg-6 col-sm-6">
+                                                          BPLA1*****
+                                                   </div>
+                                                   <div class="col-lg-6 col-sm-6">
+                                                          GST No.
+                                                   </div>
+                                                   <div class="col-lg-6 col-sm-6">
+                                                           27AATCA8561A1ZH
+                                                   </div>
+                                                   <div class="col-lg-6 col-sm-6">
+                                                           CIN No.
+                                                   </div>
+                                                   <div class="col-lg-6 col-sm-6">
+                                                          U85100MH2020PTC343851
+                                                   </div>
+                                              </div>
+                                         </div>
+                                         <div class="row">
+                                             <div class="col-sm">
+                                                   Packaging/Payment and Shipment Details 
+                                             </div>
+                                              <div class="row">
+                                                   <div class="col-lg-6 col-sm-6">
+                                                         Payment Mode
+                                                   </div>
+                                                   <div class="col-lg-6 col-sm-6">
+                                                         <p>Cash<br>Credit Card <br> Bank Transfer <br> Cheque <br> DD <br> Online <br></p>
+                                                   </div>
+                                                   <div class="col-lg-6 col-sm-6">
+                                                           Shipment Mode
+                                                   </div>
+                                                   <div class="col-lg-6 col-sm-6">
+                                                           By Road
+                                                   </div>
+                                              </div>
+                                         </div>
+                                         
                          </div>
                      </div>
                   </div>
              </div>
-          </div>
-     </section>
+     </div>
+</section>
 
-
+<?php include("footer.php"); ?>
 </body>
 </html>
